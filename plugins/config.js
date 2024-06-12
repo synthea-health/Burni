9be47f3257fa19cf9900a2fdeb0f9cd8573cac84
sankalp choudhary <sankalpchoudhary8@gmail.com>
2024-06-12 23:23:22 +0530

    fix issue

---
+++ plugins/config.js
@@ -0,0 +1,24 @@
+module.exports.pluginsConfig = {
+    tokenAuth: {
+        enable: false,
+        before: true,
+        routers: [
+            {
+                method: "get",
+                path: "/fhir/:resourceType"
+            }
+        ],
+        admin: {
+            loginPath: "",
+            username: "",
+            password: ""
+        },
+        jwt: {
+            secretKey: ""
+        }
+    },
+    checkReference: {
+        enable: true,
+        before: true
+    }
+};
